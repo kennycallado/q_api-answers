917e9af6e1a28fc767ec51731cdf1c3374ddbd27
kennycallado <kennycallado@hotmail.com>
2023-08-31 09:42:16 +0200

    CLEAN Limpia migración

--- src/database/migrations/2023-04-19-125908_create_answers/up.sql
+++ src/database/migrations/2023-04-19-125908_create_answers/up.sql
@@ -1,26 +1,10 @@
-CREATE TABLE IF NOT EXISTS answers (); -- Mandatory
+CREATE TABLE IF NOT EXISTS answers ();
 
 ALTER TABLE answers
-  -- ADD COLUMN user_id SERIAL NOT NULL,
-  -- ADD COLUMN question_type VARCHAR(10) NOT NULL,
-  -- ADD COLUMN created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  -- ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
   ADD COLUMN id SERIAL PRIMARY KEY,
   ADD COLUMN question_id SERIAL NOT NULL,
   ADD COLUMN answer VARCHAR NOT NULL
   ;
-
--- ALTER TABLE answers ADD CONSTRAINT answers_question_type_check CHECK
---   (question_type IN ('range', 'radio', 'checkbox', 'input'));
-
--- SELECT diesel_manage_updated_at('answers');
--- INSERT INTO answers (user_id, question_id, question_type, answer) VALUES
---   (1, 1, 'range', '1'),
---   (1, 2, 'range', '1'),
---   (1, 3, 'range', '3'),
---   (1, 4, 'range', '5'),
---   (1, 5, 'range', '1')
---   ;
 
 INSERT INTO answers (question_id, answer) VALUES
   (1, '1'),
@@ -29,6 +13,3 @@ INSERT INTO answers (question_id, answer) VALUES
   (4, '5'),
   (5, '1')
   ;
-
--- ALTER TABLE answers REPLICA IDENTITY FULL;
--- CREATE PUBLICATION answers_pub FOR TABLE answers;
